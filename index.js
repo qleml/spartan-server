ca1e403b29abc6a8dd042b7ae01c272c45b8416f
Clemens Christoph <cchristoph@ethz.ch>
2024-09-10 08:53:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 7051;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
